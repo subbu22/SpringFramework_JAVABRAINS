d4ccc5d2c1e36d579fcbbae4120707e2dcc5cb49
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-04 01:37:42 +0530

    Updated

--- 19th Autowire_Annotation/src/org/kishan/Circle.java
+++ 19th Autowire_Annotation/src/org/kishan/Circle.java
@@ -1,18 +1,10 @@
 package org.kishan;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
+
 import org.springframework.beans.factory.annotation.Qualifier;
 
-public class Circle implements Shape{
 
-	private Point center;
-	public Point getCenter() {
-		return center;
-	}
-	@Autowired
-	@Qualifier("circleRelated")
-=======
 
 public class Circle implements Shape{
 
@@ -21,7 +13,6 @@ public class Circle implements Shape{
 		return center;
 	}
 	@Autowired
->>>>>>> refs/remotes/origin/master
 	public void setCenter(Point center) {
 		this.center = center;
 	}
