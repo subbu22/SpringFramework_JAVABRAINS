76593c4afd1ee63f3d071a3ea79093502023bc6b
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-07 02:05:17 +0530

    First Commit

---
+++ 2 Code_Using_the_Bean_Factory/src/org/kishan/DrawingApp.java
@@ -0,0 +1,15 @@
+package org.kishan;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+	public static void main(String args[])
+	{
+		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring2.xml"));
+		Triangle triangle = (Triangle) factory.getBean("triangle");
+		triangle.draw();
+		
+	}
+}
---
+++ 2 Code_Using_the_Bean_Factory/src/org/kishan/Triangle.java
@@ -0,0 +1,9 @@
+package org.kishan;
+
+public class Triangle {
+	public void draw()
+	{
+		System.out.println("Triangle Drawn");
+	}
+
+}
