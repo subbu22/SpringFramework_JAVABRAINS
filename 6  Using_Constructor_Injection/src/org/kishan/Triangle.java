ff60e13c8b83d0110eb4777e39d88b9b25ac7af8
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-10 00:41:48 +0530

    parameters to constructor by passing "Data-Type" and by-using "index"

--- 6  Using_Constructor_Injection/src/org/kishan/Triangle.java
+++ 6  Using_Constructor_Injection/src/org/kishan/Triangle.java
@@ -9,22 +9,25 @@ public class Triangle
 	public Triangle(String type) {
 	this.type = type;
 }
+	public Triangle(int height) {
+		this.height=height;
+	}
 
 	public Triangle(String type,int height) {
 		this.type = type;
 		this.height=height;
 	}
-	//Setter-Getter method
+/*	//Setter-Getter method
 	public String getType() {
 	return type;
 }
 
-public void setType(String type) {
+	public void setType(String type) {
 	this.type = type;
-}
+}*/
 
 	public void draw() {
-		System.out.println(getType()+"triangle Drawn by Setter-Getter Method");
+		//System.out.println(getType()+"triangle Drawn by Setter-Getter Method");
 		System.out.println("triangle Drawn by Constructor : type :"+type + " " + "Height is" + " : "+height);
 
 	}
