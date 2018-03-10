f1a05bb3c7768a58899bc285b6e9cee481b6c3a8
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-11 00:47:40 +0530

    First Commit

---
+++ 11 Understanding_Bean_Scope/src/org/kishan/Triangle.java
@@ -0,0 +1,49 @@
+package org.kishan;
+
+import java.util.List;
+
+public class Triangle
+{
+	private Point pointa;
+	private Point pointb;
+	private Point pointc;
+
+	public Point getPointa() 
+	{
+		return pointa;
+	}
+
+	public void setPointa(Point pointa)
+	{
+		this.pointa = pointa;
+	}
+
+	public Point getPointb() 
+	{
+		return pointb;
+	}
+
+
+	public void setPointb(Point pointb) 
+	{
+		this.pointb = pointb;
+	}
+
+	public Point getPointc()
+	{
+		return pointc;
+	}
+
+	public void setPointc(Point pointc)
+	{
+		this.pointc = pointc;
+	}
+	
+	public void draw()
+	{
+		System.out.println("Triangle draw");
+		System.out.println(" PointA : "+"(" +pointa.getX() +","+ pointa.getY() + ")");
+		System.out.println(" PointB : "+"(" +pointb.getX() +","+ pointb.getY() + ")");
+		System.out.println(" PointC : "+"(" +pointc.getX() +","+ pointc.getY() + ")");
+	}
+}
