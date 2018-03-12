395b3d0dc956fa41f108fe552e8bb27408d3f3be
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-12 21:43:59 +0530

    Implementing interfaces like "ApplicationContextAware"&"BeanNameAware"

---
+++ 12 Using_ApplicationContextAware/src/org/kishan/DrawingApp.java
@@ -0,0 +1,15 @@
+package org.kishan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp
+{
+	public static void main(String args[])
+	{
+		System.out.println("Start of Main method");
+		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+		System.out.println("Exit from main method");
+	}
+}
