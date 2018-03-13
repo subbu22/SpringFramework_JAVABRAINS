5c9bfd2575504c92e4a847b5abf702220b0f86cb
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-13 19:36:23 +0530

    Understanding BeanPostProcessor

---
+++ 15 Writing_BeanPostProcessor/src/org/kishan/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package org.kishan;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements  BeanPostProcessor{
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("After Initialisation method : "+beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("Before Initialisation method : "+beanName);
+		return bean;
+		}
+
+}
