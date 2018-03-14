56f158f4a1fec88b721add58f2642014e0554973
Spring Framework <kishan.dhongadi@gmail.com>
2018-03-14 15:06:12 +0530

    Initial Commit
    Use of BeanfactoryPostProcessor.

---
+++ 16 BeanFactoryPostProcessor/src/org/kishan/BeanFactoryPP.java
@@ -0,0 +1,14 @@
+package org.kishan;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class BeanFactoryPP implements BeanFactoryPostProcessor {
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		System.out.println("Bean Factory PostProcessor"+beanFactory);
+		
+	}
+
+}
---
+++ 16 BeanFactoryPostProcessor/src/org/kishan/Triangle.java
@@ -0,0 +1,85 @@
+package org.kishan;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+
+//public class Triangle implements InitializingBean, DisposableBean
+public class Triangle
+{
+	private Point pointa; 
+	private Point pointb;
+	private Point pointc;
+	private ApplicationContext context;
+
+	public Point getPointa() 
+	{
+		return pointa;
+	}
+
+	public void setPointa(Point pointa)
+	{
+		this.pointa = pointa;
+	}
+
+	public Point getPointb() 
+	{
+		return pointb;
+	}
+
+
+	public void setPointb(Point pointb) 
+	{
+		this.pointb = pointb;
+	}
+
+	public Point getPointc()
+	{
+		return pointc;
+	}
+
+	public void setPointc(Point pointc)
+	{
+		this.pointc = pointc;
+	}
+	
+	public void draw()
+	{
+		System.out.println("Triangle draw");
+		System.out.println(" PointA : "+"(" +pointa.getX() +","+ pointa.getY() + ")");
+		System.out.println(" PointB : "+"(" +pointb.getX() +","+ pointb.getY() + ")");
+		System.out.println(" PointC : "+"(" +pointc.getX() +","+ pointc.getY() + ")");
+	}
+
+	/*@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Initialising Bean Init Method called");
+		
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Disposable Bean Method called");		
+	}*/
+
+	/*@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context=context;
+		
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+System.out.println("bean name is  : "+beanName);		
+	}*/
+	
+	/*public void myInit()
+	{
+		System.out.println("Init method");
+	}
+	
+	public void destroyMethod()
+	{
+		System.out.println("Destroy Method");
+	}*/
+}
